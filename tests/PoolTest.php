7fdadb793f65e7faa2170a18c3577761dfafe188
Martin Price <martin.price@emailcenteruk.com>
2015-08-27 15:09:52 +0100

    Add tests for config class, update tests for pool now that it no longer has the responsibility of handling the config

--- tests/PoolTest.php
+++ tests/PoolTest.php
@@ -2,153 +2,109 @@
 
 namespace Phlib\Logger\Test;
 
+use Phlib\Logger\Factory;
 use Phlib\Logger\Pool;
 use Psr\Log\LogLevel;
 
 class PoolTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testEmptyLogger()
+    public function testGetLogger()
     {
+        $loggerConfig = [
+            'name'    => Factory::LOGGER_COLLECTION,
+            'loggers' => []
+        ];
+
+        $config  = $this->getMock('\Phlib\Logger\Config', [], [[]]);
+        $config->expects($this->once())
+            ->method('getLoggerConfig')
+            ->with($this->equalTo('test'))
+            ->will($this->returnValue($loggerConfig));
+
         $factory = $this->getMock('\Phlib\Logger\Factory');
         $logger  = $this->getMock('\Phlib\Logger\Collection');
         $factory->expects($this->once())
             ->method('createLogger')
-            ->with(
-                $this->equalTo('test'),
-                $this->equalTo([
-                    'name'    => 'collection',
-                    'loggers' => []
-                ])
-            )
+            ->with($this->equalTo('test'), $this->equalTo($loggerConfig))
             ->will($this->returnValue($logger));
 
-        $pool = new Pool([], $factory);
+        $pool = new Pool($config, $factory);
 
         $this->assertSame($logger, $pool->test);
     }
 
-    public function testStream()
+    public function testGetLoggerAgain()
     {
-        $streamConfig = [
-            'name' => 'stream',
+        $loggerConfig = [
+            'name' => Factory::LOGGER_STREAM,
+            'path' => '(filename)'
         ];
 
-        $config = [
-            'test' => $streamConfig
-        ];
+        $config  = $this->getMock('\Phlib\Logger\Config', [], [[]]);
+        $config->expects($this->once())
+            ->method('getLoggerConfig')
+            ->with($this->equalTo('test'))
+            ->will($this->returnValue($loggerConfig));
 
         $factory = $this->getMock('\Phlib\Logger\Factory');
         $logger  = $this->getMockBuilder('\Phlib\Logger\Stream')->disableOriginalConstructor()->getMock();
         $factory->expects($this->once())
             ->method('createLogger')
-            ->with(
-                $this->equalTo('test'),
-                $this->equalTo($streamConfig)
-            )
+            ->with($this->equalTo('test'), $this->equalTo($loggerConfig))
             ->will($this->returnValue($logger));
+
         $pool = new Pool($config, $factory);
 
-        $this->assertSame($logger, $pool->test);
-    }
+        $actualLogger = $pool->test;
 
-    public function testCollectionStream()
-    {
-        $streamConfig = [
-            'name' => 'stream',
-        ];
+        $this->assertSame($logger, $actualLogger);
 
-        $config = [
-            'test' => [
-                $streamConfig
-            ]
-        ];
-
-        $factory = $this->getMock('\Phlib\Logger\Factory');
-        $logger  = $this->getMock('\Phlib\Logger\Collection');
-        $factory->expects($this->once())
-            ->method('createLogger')
-            ->with(
-                $this->equalTo('test'),
-                $this->equalTo([
-                    'name'    => 'collection',
-                    'loggers' => [$streamConfig]
-                ])
-            )
-            ->will($this->returnValue($logger));
-        $pool = new Pool($config, $factory);
-
-        $this->assertSame($logger, $pool->test);
+        $this->assertSame($actualLogger, $pool->test);
     }
 
     public function testPrefix()
     {
-        $config = [
-            'test' => [
-                [
-                    'name'  => 'stream',
-                    'level' => LogLevel::CRITICAL,
-                    'path'  => '(filename)'
-                ]
-            ]
+        $prefix = 'logger-prefix-';
+
+        $loggerConfig = [
+            'name'  => Factory::LOGGER_GELF,
+            'level' => LogLevel::CRITICAL,
+            'host'  => '(hostname)'
         ];
 
+        $config  = $this->getMock('\Phlib\Logger\Config', [], [[]]);
+        $config->expects($this->once())
+            ->method('getLoggerConfig')
+            ->with($this->equalTo('test'))
+            ->will($this->returnValue($loggerConfig));
+
         $factory = $this->getMock('\Phlib\Logger\Factory');
-        $logger  = $this->getMock('\Phlib\Logger\Collection');
+        $logger  = $this->getMock('\Phlib\Logger\Gelf');
         $factory->expects($this->once())
             ->method('createLogger')
-            ->with($this->equalTo('logger-prefix-test'))
+            ->with($this->equalTo($prefix . 'test'))
             ->will($this->returnValue($logger));
 
         $pool = new Pool($config, $factory);
-        $pool->setPrefix('logger-prefix-');
+        $pool->setPrefix($prefix);
 
         $this->assertSame($logger, $pool->test);
     }
 
-    public function testAlias()
-    {
-        $loggers = [
-            [
-                'name'  => 'stream',
-                'level' => LogLevel::CRITICAL,
-                'path'  => '(filename)'
-            ]
-        ];
-        $config = [
-            'test'  => 'test1',
-            'test1' => 'test2',
-            'test2' => $loggers
-        ];
-
-
-        $factory = $this->getMock('\Phlib\Logger\Factory');
-        $logger  = $this->getMock('\Phlib\Logger\Collection');
-        $factory->expects($this->once())
-            ->method('createLogger')
-            ->with(
-                $this->equalTo('test'),
-                $this->equalTo([
-                    'name'    => 'collection',
-                    'loggers' => $loggers
-                ])
-            )
-            ->will($this->returnValue($logger));
-
-        $pool = new Pool($config, $factory);
-
-        $this->assertSame($logger, $pool->getLogger('test'));
-    }
-
     public function testGetLoggerCollection()
     {
-        $streamConfig = [
-            'name' => 'stream',
+        $loggerConfig = [
+            'name'  => Factory::LOGGER_STREAM,
+            'level' => LogLevel::WARNING,
+            'path'  => '(filename)'
         ];
 
-        $config = [
-            'test' => $streamConfig
-        ];
+        $config  = $this->getMock('\Phlib\Logger\Config', [], [[]]);
+        $config->expects($this->once())
+            ->method('getLoggerConfig')
+            ->with($this->equalTo('test'))
+            ->will($this->returnValue($loggerConfig));
 
         $factory          = $this->getMock('\Phlib\Logger\Factory');
         $streamLogger     = $this->getMockBuilder('\Phlib\Logger\Stream')->disableOriginalConstructor()->getMock();
@@ -157,7 +113,7 @@ class PoolTest extends \PHPUnit_Framework_TestCase
             ->method('createLogger')
             ->with(
                 $this->equalTo('test'),
-                $this->equalTo($streamConfig)
+                $this->equalTo($loggerConfig)
             )
             ->will($this->returnValue($streamLogger));
         $factory->expects($this->once())
